b2e76163bb1ff116128494c92166e87a70077601
attilak125 <36953017+attilak125@users.noreply.github.com>
2018-04-13 18:42:07 +0200

    Első nap második próbálkozás, hogyan induljon el az egyezés keresésének a folyamata

--- 8_feladat.py
+++ 8_feladat.py
@@ -1,6 +1,24 @@
 def kreses(a,b):
-    max=0
-    for i in range(0,len(a)-1):
-        for j in range(0,len(b)-1):
+    maxegyezes=0
+    if(len(a)>len(b))
+        min=len(b)-1
+        max=len(a)-1
+    for i in range(0,max):
+        for j in range(0,min):
             if(a[i]==b[j]):
-                
+                count=0
+                aindex = i
+                bindex = j
+                while(1):
+                    if(aindex<max|bindex<min):
+                        if(a[aindex]==b[bindex]):
+                            count+=1
+                        elif(maxegyezes<count):
+                            maxegyezes=count
+                            count=0
+                            break
+                        else:
+                            count=0
+                            break
+                    aindex+=1
+                    bindex+=1
