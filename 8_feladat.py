3e861c82b3acfc101d5ac6e2b5b206688eb3b678
attilak125 <36953017+attilak125@users.noreply.github.com>
2018-04-15 13:45:25 +0200

    Első nap második próbálkozás, hogyan induljon el az egyezés keresésének a folyamata

--- 8_feladat.py
+++ 8_feladat.py
@@ -1,25 +1,49 @@
-def kreses(a,b):
+def kereses(a,b):
     maxegyezes=0
-    if(len(a)>len(b))
-        min=len(b)-1
-        max=len(a)-1
-    for i in range(0,max):
-        for j in range(0,min):
-            if(a[i]==b[j]):
-                count=0
-                aindex = i
-                bindex = j
-                while(1):
-                    if(aindex<max|bindex<min):
-                        if(a[aindex]==b[bindex]):
-                            count+=1
-                        elif(maxegyezes<count):
+    if(len(a)>len(b)):
+        min=len(b)
+        max=len(a)
+        for i in range(0, max):
+            for j in range(0, min):
+                if (a[i] == b[j]):
+                    count = 0
+                    if(max-i>min-j):
+                        aindex = i
+                        for bindex in range(j,min):
+                            if(a[aindex]==b[bindex]):
+                                count+=1
+                                print(count,maxegyezes)
+                            elif(count>maxegyezes):
+                                maxegyezes=count
+                                break
+                            else:
+                                break
+                            aindex += 1
+                        if(maxegyezes<count):
                             maxegyezes=count
-                            count=0
-                            break
-                        else:
-                            count=0
-                            break
-                    aindex+=1
-                    bindex+=1
-                    
+
+    # else:
+    #     max=len(b)
+    #     min=len(a)
+    #     for i in range(0,max):
+    #         for j in range(0,min):
+    #             if(a[j]==b[i]):
+    #                 count=0
+    #                 aindex = j
+    #                 bindex = i
+    #                 while(1):
+    #                     if(aindex<=max|bindex<=min):
+    #                         if(a[aindex]==b[bindex]):
+    #                             count+=1
+    #                         elif(maxegyezes<count):
+    #                             maxegyezes=count
+    #                             count=0
+    #                             break
+    #                         else:
+    #                             count=0
+    #                             break
+    #                     aindex+=1
+    #                     bindex+=1
+    print(maxegyezes)
+    return 0
+kereses("Az egy csirke a placon","csirke")
