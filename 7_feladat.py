c529b20a637ea785edf704233da2dd797957e89c
attilak125 <36953017+attilak125@users.noreply.github.com>
2018-05-03 12:21:18 +0200

    0

--- 7_feladat.py
+++ 7_feladat.py
@@ -7,59 +7,70 @@ def palindromic(a):
     maxegyezes=''
     szamlalo=0
     maximum=0
+    egyezeshelye=0
     for i in b:
         if 'a' <= i <= 'z':
             normalstring+=i
     for i in range (len(normalstring)-1,-1,-1):
         reverstring+=normalstring[i]
+    print(reverstring, normalstring)
 
     for i in range(0,len(normalstring)):
-        if normalstring[i] == reverstring[i]:
-            egyezes+=reverstring[i]
-            szamlalo+=1
-        elif szamlalo > maximum:
-            maxegyezes=egyezes
-            maximum=szamlalo
-            egyezes=''
-            szamlalo=0
-        else:
-            egyezes=''
-            szamlalo=0
+        for j in range(egyezeshelye,len(reverstring)):
+            if normalstring[i] == reverstring [j]:
+                print(normalstring[i],reverstring[j])
+                if i > j:
+                    print(1)
+                    seged = i-j
+                    segedindex=0
+                    for index in range(i,len(reverstring)):
+                        segedindex= index-seged
+                        if normalstring[index] == reverstring[segedindex]:
+                            egyezes+=normalstring[index]
+                            szamlalo+=1
+                        elif szamlalo > maximum:
+                            maxegyezes = egyezes
+                            maximum = szamlalo
+                            szamlalo=0
+                            egyezes=''
+                        else:
+                            szamlalo=0
+                            egyezes=''
+                else:
+                    seged=j-i
+                    segedindex=0
+                    for index in range(j,len(reverstring)):
+                        segedindex=index-seged
+                        if normalstring[segedindex] == reverstring[index]:
+                            egyezes+=normalstring[index]
+                            szamlalo+=1
+                        elif szamlalo > maximum:
+                            maxegyezes = egyezes
+                            maximum = szamlalo
+                            szamlalo=0
+                            egyezes=''
+                        else:
+                            szamlalo=0
+                            egyezes=''
+            elif szamlalo > maximum:
+                maxegyezes = egyezes
+                maximum = szamlalo
+                szamlalo = 0
+                egyezes = ''
+            else:
+                szamlalo = 0
+                egyezes = ''
+
     if szamlalo > maximum:
         maxegyezes=egyezes
         maximum=szamlalo
     print(maxegyezes)
     print(maximum)
 
-
-    # seged=''
-    # world=''
-    # num=0
-    # max=0
-    # for ch in b:
-    #     if 'a'<= ch <= 'z':
-    #         world+=ch
-    #     else:
-    #         for i in range (len(world)-1,-1,-1):
-    #             seged+=world[i]
-    #         seged+=ch
-    #         world=''
-    # for i in range(len(world) - 1, -1, -1):
-    #     seged += world[i]
-    # seged += ch
-    # for i in range(0,len(b)-1):
-    #     if(seged[i]==b[i]):
-    #         num+=1
-    #     elif(num>max):
-    #         max=num
-    #         num=0
-    #     else:
-    #         num=0
-    # print(max)
     return 0
 
 
 
 
 
-palindromic("a gorog a")
+palindromic(" almafakecske a gorog a banas")
