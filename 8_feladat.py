ee2049c9fade35957dccbb6f804dd5f61fc80741
attilak125 <36953017+attilak125@users.noreply.github.com>
2018-05-09 14:23:14 +0200

    0

--- 8_feladat.py
+++ 8_feladat.py
@@ -1,97 +1,12 @@
-def kereses(a,b):
-    maxegyezes=0
-    if len(a) > len(b):
-        hosszabb=a
-        rovidebb=b
+def lcs(X, Y, m, n):
+    if m == 0 or n == 0:
+        return 0;
+    elif X[m - 1] == Y[n - 1]:
+        return 1 + lcs(X, Y, m - 1, n - 1);
     else:
-        hosszabb=b
-        rovidebb=a
-    for i in range (0,len(rovidebb)):
-        for j in range(0,len(hosszabb)):
-            if rovidebb[i] == hosszabb[j]:
-                for inedx in range (i,len(rovidebb)):
-                    if rovidebb[inedx] == hosszabb[j]:
-                        maxegyezes+=1
-                        if j < len(hosszabb):
-                            j+=1
+        return max(lcs(X, Y, m, n - 1), lcs(X, Y, m - 1, n));
 
-                    else:
-                        break
-    print(maxegyezes)
-    return 0
-
-
-
-
-
-#     maxegyezes=0
-#     if(len(a)>len(b)):
-#         min=len(b)
-#         max=len(a)
-#         for i in range(0, max):
-#             for j in range(0, min):
-#                 if (a[i] == b[j]):
-#                     count = 0
-#                     if(max-i>=min-j):
-#                         aindex = i
-#                         for bindex in range(j,min):
-#                             if(a[aindex]==b[bindex]):
-#                                 count+=1
-#                             elif(count>maxegyezes):
-#                                 maxegyezes=count
-#                                 break
-#                             else:
-#                                 break
-#                             aindex += 1
-#                         if(maxegyezes<count):
-#                             maxegyezes=count
-#                     else:
-#                         bindex = j
-#                         for aindex in range(i,max):
-#                             if (a[aindex] == b[bindex]):
-#                                 count += 1
-#                             elif (count > maxegyezes):
-#                                 maxegyezes = count
-#                                 break
-#                             else:
-#                                 break
-#                             bindex += 1
-#                         if (maxegyezes < count):
-#                             maxegyezes = count
-#
-#     else:
-#         min=len(a)
-#         max=len(b)
-#         for i in range(0, max):
-#             for j in range(0, min):
-#                 if (a[j] == b[i]):
-#                     count = 0
-#                     if(max-i>=min-j):
-#                         bindex = i
-#                         for aindex in range(j,min):
-#                             if(a[aindex]==b[bindex]):
-#                                 count+=1
-#                             elif(count>maxegyezes):
-#                                 maxegyezes=count
-#                                 break
-#                             else:
-#                                 break
-#                             bindex += 1
-#                         if(maxegyezes<count):
-#                             maxegyezes=count
-#                     else:
-#                         aindex = j
-#                         for bindex in range(i,max):
-#                             if (a[aindex] == b[bindex]):
-#                                 count += 1
-#                             elif (count > maxegyezes):
-#                                 maxegyezes = count
-#                                 break
-#                             else:
-#                                 break
-#                             aindex += 1
-#                         if (maxegyezes < count):
-#                             maxegyezes = count
-#     print(maxegyezes)
-#     return maxegyezes
-kereses("apple","tableteeeee")
+X = "apple"
+Y = "tablep"
+eredmeny = lcs(X , Y, len(X), len(Y))
+print( eredmeny)
